e985c0d2e5b25798618fe6366d6179377764f752
Skyblueeeeeeeeeee <132131791+Skyblueeeeeeeeeee@users.noreply.github.com>
2023-11-30 15:30:27 +0700

    Update page.tsx

--- app/(root)/(routes)/(dashboard)/page.tsx
+++ app/(root)/(routes)/(dashboard)/page.tsx
@@ -10,7 +10,7 @@ import { TbEntity } from "thingsboard-api-client";
 import HistoryTable from "./components/history-table";
 import TelemetryTable from "./components/telemetry-table";
 
-const deviceId = "ef3dc260-8a21-11ee-ace3-afcb18c2767c";
+const deviceId = "704147a0-82b2-11ee-9abd-09c043ac25d0";
 const keys = "latitude,longitude";
 
 const DashboardPage = () => {
